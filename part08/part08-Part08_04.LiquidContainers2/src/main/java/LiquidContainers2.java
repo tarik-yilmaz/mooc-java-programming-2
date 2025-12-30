c6560b20ade2afdba13bc10e740fed5a184093fa
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-12-30 11:15:17 +0100

    added part08-04

---
+++ part08/part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+public class LiquidContainers2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Container firstContainer = new Container();
+        Container secondContainer = new Container();
+
+        while (true) {
+            System.out.println("First: " + firstContainer.toString());
+            System.out.println("Second: " + secondContainer.toString());
+            System.out.print("> ");
+
+            String input = scan.nextLine();
+            if (input.equals("quit")) {
+                break;
+            }
+
+            String[] parts = input.split(" ");
+
+            String command = parts[0].toLowerCase();
+
+            int amount = Integer.valueOf(parts[1]);
+
+            if (command.equals("add")) {
+                firstContainer.add(amount);
+            
+            } else if (command.equals("remove")) {
+                secondContainer.remove(amount);
+            
+            } else if (command.equals("move")) {
+                if (amount > firstContainer.contains()) {
+                    secondContainer.add(firstContainer.contains());
+                    firstContainer.remove(amount);
+                } else {
+                    firstContainer.remove(amount);
+                    secondContainer.add(amount);
+                }
+            }
+        }
+    }
+}
