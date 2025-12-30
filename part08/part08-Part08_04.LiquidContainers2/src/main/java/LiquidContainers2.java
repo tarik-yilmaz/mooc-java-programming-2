c437398a05a740eba81f0b19be34b451363c531a
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-12-30 12:19:52 +0100

    update exercise

--- part08/part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part08/part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -39,6 +39,8 @@ public class LiquidContainers2 {
                     firstContainer.remove(amount);
                     secondContainer.add(amount);
                 }
+            } else {
+                continue;
             }
         }
     }
