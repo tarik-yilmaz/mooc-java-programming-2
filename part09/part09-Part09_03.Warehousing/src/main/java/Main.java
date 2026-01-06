abbfbd00fbc48963486a5785f78f939c20c8c4a5
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2026-01-06 13:58:17 +0100

    update part09-03

--- part09/part09-Part09_03.Warehousing/src/main/java/Main.java
+++ part09/part09-Part09_03.Warehousing/src/main/java/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
         // here you can write code to test your classes
 
-        ProductWarehouse juice = new ProductWarehouse("Juice", 1000.0);
-        juice.addToWarehouse(1000.0);
+        ProductWarehouseWithHistory juice = new ProductWarehouseWithHistory("Juice", 1000.0, 1000.0);
         juice.takeFromWarehouse(11.3);
-        System.out.println(juice.getName()); // Juice
         juice.addToWarehouse(1.0);
-        System.out.println(juice);    
+        //System.out.println(juice.history()); // [1000.0, 988.7, 989.7]
+
+        juice.printAnalysis();
     }
 
 }
