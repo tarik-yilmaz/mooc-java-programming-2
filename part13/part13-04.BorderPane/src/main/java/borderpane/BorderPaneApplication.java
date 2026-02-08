f25936d53b649761b91f9603398b1c7d6c2a95d7
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2026-02-08 20:31:54 +0100

    update exercise

--- part13/part13-04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
+++ part13/part13-04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
@@ -1,11 +1,32 @@
 package borderpane;
-
-
-public class BorderPaneApplication {
-
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+ 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+ 
+public class BorderPaneApplication extends Application {
+ 
+    @Override
+    public void start(Stage window) {
+        BorderPane layout = new BorderPane();
+        
+        layout.setTop(new Label("NORTH"));
+        layout.setRight(new Label("EAST"));
+        layout.setLeft(new Label("WEST"));
+        layout.setBottom(new Label("SOUTH"));
+        
+        Scene view = new Scene(layout);
+        
+        window.setScene(view);
+        window.show();
     }
-
+    
+    
+    public static void main(String[] args) {
+        launch(BorderPaneApplication.class);
+    }
+ 
 }
+ 
