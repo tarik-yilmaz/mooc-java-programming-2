997770115cedd3dc57a8ae71cd9ea966ce37c0ca
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2026-02-08 20:32:19 +0100

    update exercise

--- part13/part13-05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
+++ part13/part13-05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,41 @@
 package textstatistics;
-
-
-public class TextStatisticsApplication {
-
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+ 
+import javafx.application.Application;
+import javafx.scene.control.TextArea;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+ 
+ 
+public class TextStatisticsApplication extends Application {
+    
+    @Override
+    public void start(Stage window) throws Exception {
+        
+        BorderPane layout = new BorderPane();
+        
+        HBox hBox = new HBox();
+        
+        hBox.setSpacing(10);
+        hBox.getChildren().add(new Label("Letters: 0"));
+        hBox.getChildren().add(new Label("Words: 0"));
+        hBox.getChildren().add(new Label("The longest word is: "));
+        
+        layout.setCenter(new TextArea(""));
+        layout.setBottom(hBox);
+        
+        Scene view = new Scene(layout);
+        
+        window.setScene(view);
+        window.show();
     }
-
+    
+    
+    public static void main(String[] args) {
+        launch(TextStatisticsApplication.class);
+    }
+ 
 }
+ 
