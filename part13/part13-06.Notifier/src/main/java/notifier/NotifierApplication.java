80ac894663cb15df8cd500fa876e82bf59d7f2cd
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2026-02-08 20:32:45 +0100

    update exercise

--- part13/part13-06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ part13/part13-06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,11 +1,38 @@
 package notifier;
-
-
-public class NotifierApplication {
-
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+ 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+ 
+ 
+public class NotifierApplication extends Application {
+ 
+    @Override
+    public void start(Stage window) throws Exception {
+        
+        TextField upperText = new TextField("Hello World!");
+        Label lowerText = new Label();
+        Button button = new Button("Update");
+        
+        button.setOnAction(event -> lowerText.setText(upperText.getText()));
+        
+        VBox componentGroup = new VBox();
+        componentGroup.getChildren().addAll(upperText, button, lowerText);
+        
+        Scene view = new Scene(componentGroup);
+        
+        window.setScene(view);
+        window.show();
+        
     }
-
+    
+    public static void main(String[] args) {
+        launch(NotifierApplication.class);
+    }
+ 
 }
+ 
