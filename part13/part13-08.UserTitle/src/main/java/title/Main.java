349b0e6d936f4ccf6f3a5519b448399c3e7bd8d6
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2026-02-08 20:34:19 +0100

    update exercise

--- part13/part13-08.UserTitle/src/main/java/title/Main.java
+++ part13/part13-08.UserTitle/src/main/java/title/Main.java
@@ -1,11 +1,21 @@
 package title;
-
-
+ 
+import java.util.Scanner;
+import javafx.application.Application;
+ 
+ 
 public class Main {
-
+ 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Name a title for the application: ");
+        String title = scanner.nextLine();
+        
+        Application.launch(UserTitle.class, "--title=", title);
+        
+        scanner.close();
     }
-
+ 
 }
+ 
